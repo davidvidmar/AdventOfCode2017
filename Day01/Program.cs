eef0a9fa4b09b24a5f80977a1e980f4b9a1a2253
David Vidmar <info@vidmar.net>
2017-12-02 22:17:04 +0100

    Day 1 and 2

---
+++ Day01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Advent of Code 2017 - Day 1\n");
+
+        SolveCaptcha("1122");
+        SolveCaptcha("1111");
+        SolveCaptcha("1234");
+        SolveCaptcha("91212129");
+
+        var input = File.ReadAllText("input.txt");
+        SolveCaptcha(input);
+    }
+
+    private static void SolveCaptcha(string input)
+    {
+        var result = 0;
+        var num = 0;
+
+        int i = 1;
+        while (i < input.Length)
+        {
+            if (input[i] == input[i - 1])
+            {
+                num = Convert.ToInt32(input[i].ToString());
+                Console.Write(num + "+");
+                result += num;
+            }                
+            i++;
+        }
+
+        if (input[i - 1] == input[0])
+        {
+            num = Convert.ToInt32(input[i-1].ToString());
+            Console.Write(num);
+            result += num;
+        }
+            
+        Console.WriteLine($"\n---\nResult: {result}\n");
+    }                     
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {        
+        Console.WriteLine("Advent of Code 2017 - Day 2\n");
+
+        CalcChecksum(new[] { "5\t1\t9\t5", "7\t5\t3", "2\t4\t6\t8" });
+
+        var input = File.ReadAllLines("input.txt");
+        CalcChecksum(input);
+    }
+
+    private static void CalcChecksum(string[] lines)
+    {
+        var result = 0;
+        foreach (var line in lines)
+        {            
+            var nums = line.Split(new[] { "\t" }, StringSplitOptions.RemoveEmptyEntries);
+            var min = Convert.ToInt32(nums[0]);
+            var max = Convert.ToInt32(nums[0]);
+            foreach (var num in nums)
+            {
+                var n = Convert.ToInt32(num);
+                if (n < min) min = n;
+                if (n > max) max = n;
+            }
+            var diff = max - min;
+            Console.Write(diff + "+");
+            result += max - min;
+        }
+        Console.WriteLine($"\n---\nResult: {result}\n");        
+    }
+}
