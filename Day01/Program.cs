fa839e9bbdc8acbc492d723b1b7bad8717f135ef
David Vidmar <info@vidmar.net>
2017-12-02 23:27:20 +0100

    Part 1 for Day 1 and 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -5,41 +5,80 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Advent of Code 2017 - Day 1\n");
-
-        SolveCaptcha("1122");
-        SolveCaptcha("1111");
-        SolveCaptcha("1234");
-        SolveCaptcha("91212129");
+        Console.WriteLine("Advent of Code 2017 - Day 1\n\nPart 1\n");
 
         var input = File.ReadAllText("input.txt");
+
+        SolveCaptcha("1122", 3);
+        SolveCaptcha("1111", 4);
+        SolveCaptcha("1234", 0);
+        SolveCaptcha("91212129", 9);
+                
         SolveCaptcha(input);
+
+        Console.WriteLine("\nPart 2\n");
+
+        SolveCaptcha2("1212", 6);
+        SolveCaptcha2("1221", 0);
+        SolveCaptcha2("123425", 4);
+        SolveCaptcha2("123123", 12);
+        SolveCaptcha2("12131415", 4);
+
+        SolveCaptcha2(input);
     }
 
-    private static void SolveCaptcha(string input)
+    private static void SolveCaptcha(string input, int? solution = null)
     {
         var result = 0;
         var num = 0;
 
-        int i = 1;
+        var input2 = input + input;
+
+        int i = 0;
         while (i < input.Length)
         {
-            if (input[i] == input[i - 1])
+            if (input[i] == input2[i + 1])
             {
-                num = Convert.ToInt32(input[i].ToString());
+                num = Int32.Parse(input[i].ToString());
                 Console.Write(num + "+");
                 result += num;
             }                
             i++;
         }
+                    
+        Console.Write($"\nResult: {result}");
 
-        if (input[i - 1] == input[0])
+        if (solution != null) 
+            Console.Write(result == solution ? " - CORRECT!" : $"NOPE! Should be {solution}");
+
+        Console.WriteLine("\n");
+    }
+    
+    private static void SolveCaptcha2(string input, int? solution = null)
+    {
+        var result = 0;
+        var num = 0;
+
+        var input2 = input + input;
+
+        int i = 0;
+        while (i < input.Length)
         {
-            num = Convert.ToInt32(input[i-1].ToString());
-            Console.Write(num);
-            result += num;
+            if (input[i] == input2[i + input.Length / 2])
+            {
+                num = Int32.Parse(input[i].ToString());
+                Console.Write(num + "+");
+                result += num;
+            }
+            i++;
         }
-            
-        Console.WriteLine($"\n---\nResult: {result}\n");
-    }                     
+
+        Console.Write($"\nResult: {result}");
+        
+        if (solution != null)
+            Console.Write(result == solution ? " - CORRECT!" : $" - NOPE! Should be {solution}");
+
+        Console.WriteLine("\n");
+    }
+
 }
--- Day02/Program.cs
+++ Day02/Program.cs
@@ -4,26 +4,31 @@ using System.IO;
 class Program
 {
     static void Main(string[] args)
-    {        
-        Console.WriteLine("Advent of Code 2017 - Day 2\n");
-
-        CalcChecksum(new[] { "5\t1\t9\t5", "7\t5\t3", "2\t4\t6\t8" });
+    {
+        Console.WriteLine("Advent of Code 2017 - Day 2\n\nPart 1\n");
 
         var input = File.ReadAllLines("input.txt");
+
+        CalcChecksum(new[] { "5\t1\t9\t5", "7\t5\t3", "2\t4\t6\t8" }, 18);
         CalcChecksum(input);
+
+        Console.WriteLine("\nPart 2\n");
+
+        CalcEvenDivide(new[] { "5\t9\t2\t8", "9\t4\t7\t3", "3\t8\t6\t5" }, 9);
+        CalcEvenDivide(input);
     }
 
-    private static void CalcChecksum(string[] lines)
+    private static void CalcChecksum(string[] lines, int? solution = null)
     {
         var result = 0;
         foreach (var line in lines)
         {            
             var nums = line.Split(new[] { "\t" }, StringSplitOptions.RemoveEmptyEntries);
-            var min = Convert.ToInt32(nums[0]);
-            var max = Convert.ToInt32(nums[0]);
+            var min = Int32.Parse(nums[0]);
+            var max = Int32.Parse(nums[0]);
             foreach (var num in nums)
             {
-                var n = Convert.ToInt32(num);
+                var n = Int32.Parse(num);
                 if (n < min) min = n;
                 if (n > max) max = n;
             }
@@ -31,6 +36,43 @@ class Program
             Console.Write(diff + "+");
             result += max - min;
         }
-        Console.WriteLine($"\n---\nResult: {result}\n");        
+
+        Console.Write($"\nResult: {result}");
+
+        if (solution != null)
+            Console.Write(result == solution ? " - CORRECT!" : $"NOPE! Should be {solution}");
+
+        Console.WriteLine("\n");
+    }
+
+    private static void CalcEvenDivide(string[] lines, int? solution = null)
+    {
+        var result = 0;
+        foreach (var line in lines)
+        {
+            var nums = line.Split(new[] { "\t" }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j) continue;
+                    var numi = Double.Parse(nums[i]);
+                    var numj = Double.Parse(nums[j]);
+                    if (numi / numj == (int)(numi / numj))
+                    {
+                        result += (int)(numi / numj);
+                        break;  
+                    }                    
+                }
+            }            
+        }
+
+        Console.Write($"Result: {result}");
+
+        if (solution != null)
+            Console.Write(result == solution ? " - CORRECT!" : $"NOPE! Should be {solution}");
+
+        Console.WriteLine("\n");
     }
 }
